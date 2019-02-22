4f6d63ce5dca9e795080951790236b594a3a4345
MichaelCourter <michael.courter.64@gmail.com>
2019-02-22 10:08:01 -0800

    New Java project

---
+++ CSS337WalletAssignment/src/css337walletassignment/CSS337WalletAssignment.java
@@ -0,0 +1,16 @@
+package css337walletassignment;
+
+/**
+ *
+ * @author Michael Courter, Mani Majd
+ */
+public class CSS337WalletAssignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
